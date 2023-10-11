0f25557877a45f1cb72d9a3e1870c907dfc2df41
JustGodWork <discord1elie@gmail.com>
2023-10-11 05:03:34 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
+/* 
+ * Print '"Programming is like building a multilingual puzzle' 
+ */
+int main(void) 
+{
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
