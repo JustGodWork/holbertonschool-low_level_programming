49e4a520a67790e7cf1c5d4356cc4a4bd35808ec
JustGodWork <discord1elie@gmail.com>
2023-10-16 01:45:06 -0700

    1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,11 @@ int main(void)
 
 	if (lastDigit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf(
+				"Last digit of %d is %d and is greater than 5\n", 
+				n, 
+				lastDigit
+		);
 	};
 
 	if (lastDigit == 0)
