f46396f44ef22abb03c557f5fbb6f28ef3b5a2ae
JustGodWork <discord1elie@gmail.com>
2023-10-17 05:00:41 -0700

    8-print_base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Base16'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hexDigit = '0';
+
+	while (hexDigit <= '9')
+	{
+		putchar(hexDigit);
+		hexDigit++;
+	};
+
+	putchar('\n');
+
+	return (0);
+}
