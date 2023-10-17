667da070ed9525a3077aff39e0f8400dd41723ed
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-10-17 21:03:37 +0200

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
