702cb69879976d519607652e1a87ef7f822fb462
JustGodWork <discord1elie@gmail.com>
2023-10-17 05:15:27 -0700

    9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'All digit'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar('0' + digit);
+		if (digit < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		};
+	};
+
+	return (0);
+}
