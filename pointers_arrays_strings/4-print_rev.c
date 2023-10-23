8378e205bacdbed5de4e15cb6bab78a790d92f1f
JustGod <discord1elie@gmail.com>
2023-10-23 10:00:04 +0200

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0, index;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+
+	for (index = len - 1; index >= 0; index--)
+	{
+		_putchar(*(s + index));
+	}
+
+	_putchar('\n');
+}
