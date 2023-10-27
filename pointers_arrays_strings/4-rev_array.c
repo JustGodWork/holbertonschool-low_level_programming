1bd0be617e1bc3109fdeabae5a967786738a5552
JustGod <discord1elie@gmail.com>
2023-10-27 09:40:46 +0200

    4-rev_array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,19 +2,23 @@
 
 /**
  * reverse_array - I am a kind of paranoid in reverse.
- * @a Array to reverse
- * @n Array length
+ * @a: Array to reverse
+ * @n: Array length
  * Return: void
 */
 void reverse_array(int *a, int n)
 {
 	int i, tmp;
-	const int last_index = n - 1; /* Save array last index to variable */
+	/* Save array last index to variable */
+	const int last_index = n - 1;
 
 	for (i = 0; i < (n / 2); i++)
 	{
-		tmp = a[last_index - i]; /* Save current last array value to tmp variable */
-		a[last_index - i] = a[i]; /* Set current last array value to current array index value */
-		a[i] = tmp; /* Set current array index value to tmp value */
+		/* Save current last array value to tmp variable */
+		tmp = a[last_index - i];
+		/* Set current last array value to current array index value */
+		a[last_index - i] = a[i];
+		/* Set current array index value to tmp value */
+		a[i] = tmp;
 	}
 }
