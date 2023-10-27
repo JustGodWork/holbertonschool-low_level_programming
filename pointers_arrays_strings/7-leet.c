8a9e6a43475b8005f3a1ffe6e5b8c4f4191d9cb2
JustGod <discord1elie@gmail.com>
2023-10-27 11:43:26 +0200

    7-leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to be encoded
+ *
+ * Return: pointer to the resulting string
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	char *letters = "aAeEoOtTlL";
+	char *numbers = "4433007711";
+
+	while (s[i])
+	{
+		for (j = 0; letters[j]; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
