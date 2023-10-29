7d5d7364f2462694aeb8fea92fef3a38c8f4bd43
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-10-29 11:37:27 +0100

    0-memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - Fill memory with a constant byte.
+ * @s:  String to fill
+ * @b: Byte used to fill @s
+ * @n: Number of bytes to fill
+ * Return: char*
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (s[i] && i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+}
