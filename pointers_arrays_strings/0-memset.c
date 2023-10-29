7543c7de163a44f4e21195263b4dd5ccc7b98d2b
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-10-29 12:04:41 +0100

    0-memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -11,12 +11,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	while (s[i])
+	while (i < (int)n)
 	{
-		if (i < (int)n)
-			s[i] = b;
-		else
-			break;
+		s[i] = b;
 		i++;
 	}
 	return (s);
