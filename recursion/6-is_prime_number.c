3dc4beea903318ba075fc410586df6a667a693d5
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-11-01 16:59:59 +0100

    6-is_prime_number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * is_prime_number - checks if a number is prime
- * @n: the number to check
- *
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime_number(int n)
-{
-	if (n <= 1)
-		return (0);
-	return (check_prime(n, 2));
-}
-
-/**
  * check_prime - checks if a number is prime recursively
  * @n: the number to check
  * @i: the current divisor to check
@@ -28,3 +15,17 @@ int check_prime(int n, int i)
 		return (0);
 	return (check_prime(n, i + 1));
 }
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: the number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, 2));
+}
+
