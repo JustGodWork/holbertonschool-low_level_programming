b85d8aa7bc41a4abf76af212a19f505efa4aaa72
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-11-01 16:49:39 +0100

    0-puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,22 +9,26 @@
 void _puts_recursion(char *s)
 {
 
-    /* if string (s) pointer is char (NULL) */
-    /* return (void) */
-    _putchar(*(s));
+	/* if string (s) pointer is char (NULL) */
+	/* return (void) */
+	if (*(s + 1))
+	{
+		/* call _puts_recursion function with */
+		/* incremented pointer (s + 1) */
+		/* to print the next character in the string */
+		s++;
+		_puts_recursion(s);
+		return;
+	}
+	else
+	{
+		if (*(s + 1) != '\0')
+		{
+			_putchar(*s);
+			return;
+		}
+	}
 
-    /* if string (s) pointer is char (NULL) */
-    /* return (void) */
-    if (*(s + 1))
-    {
-        /* call _puts_recursion function with */
-        /* incremented pointer (s + 1) */
-        /* to print the next character in the string */
-        s++;
-        _puts_recursion(s);
-        return;
-    }
-
-    _putchar('\n');
+	_putchar('\n');
 
 }
