49723a38d459ad2cd99ddbefc6dab934a0776754
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-11-03 19:40:13 +0100

    0-whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
