dde36859af6dc82899a8933a5e0a130cf7be581a
JustGod <discord1elie@gmail.com>
2023-11-13 09:41:49 +0100

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
