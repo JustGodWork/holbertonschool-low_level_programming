5eeca1a34904e7463f87a4bf326b15f851cb43e9
JustGod <discord1elie@gmail.com>
2023-11-16 10:56:08 +0100

    function_pointers

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
-#ifndef FUNC_H
-#define FUNC_H
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
