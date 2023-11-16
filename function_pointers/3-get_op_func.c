3b26630ece8a39e09c9d04a520e8fc126f63c45f
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-11-16 18:29:15 +0100

    3-get_op_func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -25,12 +25,12 @@ int (*get_op_func(char *s))(int, int)
 	int i = 0;
 
 	/* if the string is not 1 char long */
-	if (strlen(s) != 1)
+	/** if (strlen(s) != 1)
 	{
 		printf("Error\n");
 		exit(99);
 		return (NULL);
-	}
+	} */
 	/* while element of the struct exist */
 	while (ops[i].op != NULL)
 	{
