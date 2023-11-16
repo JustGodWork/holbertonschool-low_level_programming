455a57d5a8fc16bde4d9d8d43b1780ae6e0c5f64
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-11-16 17:44:05 +0100

    fix:(Project 3)

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,11 @@
 #ifndef VAR_FUNC_H
 #define VAR_FUNC_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
