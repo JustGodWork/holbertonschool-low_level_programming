31cc1a63638710030b269d926f3325110134c20b
JustGod <discord1elie@gmail.com>
2023-11-16 10:54:21 +0100

    0-print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: function to print name
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
  *
- * Return: void
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name && f)
+	f(name);
 }
