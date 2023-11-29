1a590f319cda868b02165044b5da27bde5c9b722
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-11-29 09:44:14 +0100

    Fix singly_linked_list dup_str

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,6 +9,7 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	char *dup_str;
 	list_t *new_node;
 	size_t len = 0;
 
@@ -20,7 +21,15 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
+	dup_str = strdup(str);
+
+	if (dup_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = dup_str;
 	new_node->len = len;
 	new_node->next = *head;
 
