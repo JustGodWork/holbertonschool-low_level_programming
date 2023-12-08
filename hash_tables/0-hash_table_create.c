d6b247bd51928c96629285913ed27a01e111af1b
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-12-08 15:01:14 +0100

    0-hash_table_create

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,19 +12,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
 	if (!new_table)
-	    return NULL;
+		return (NULL);
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!new_table->array)
 	{
-	    free(new_table);
-	    return (NULL);
+		free(new_table);
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
-	    new_table->array[i] = NULL;
+		new_table->array[i] = NULL;
 
 	return (new_table);
 }
