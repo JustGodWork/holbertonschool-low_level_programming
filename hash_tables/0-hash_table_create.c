4dea88e2eb09b6f922dacb14a0c7e7bd7e42b0c8
JustGod <85418813+JustGodWork@users.noreply.github.com>
2023-12-08 16:57:40 +0100

    3-hash_table_set

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
 	if (!new_table)
-		return NULL;
+		return (NULL);
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
