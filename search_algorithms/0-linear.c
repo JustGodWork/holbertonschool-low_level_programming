f3e6fc210bc3df34e1c17b8a316cd1554651dfc8
JustGod <85418813+JustGodWork@users.noreply.github.com>
2024-01-19 08:46:25 +0100

    Fixed variable declaration

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,10 +12,12 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+
 	if (array == NULL)
 		return (-1);
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
